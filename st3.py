cec98fe0a32d5e43cd8feb2ef45be3f4753a23f6
AntoineBWild <blancheantoine@yahoo.fr>
2024-12-03 09:32:20 +0100

    Update st3.py

--- st3.py
+++ st3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-#Données des utilisateurs en dur dans le code
 users_data = {
     "root": {
         "password": "rootMDP",
@@ -19,7 +18,11 @@ users_data = {
     }
 }
 
-#Fonction d'authentification
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+if "username" not in st.session_state:
+    st.session_state["username"] = ""
+
 def authenticate(username, password):
     if username in users_data:
         user = users_data[username]
@@ -31,7 +34,6 @@ def authenticate(username, password):
             return False
     return False
 
-#Afficher la page d'authentification
 def login_page():
     st.title("Login")
     username = st.text_input("Username")
@@ -47,3 +49,39 @@ def login_page():
                 st.error("Nom d'utilisateur ou mot de passe incorrect")
         else:
             st.warning("Les champs username et mot de passe doivent être remplis")
+
+def main_menu():
+    with st.sidebar:
+        st.write(f"Bienvenue, {st.session_state['username']}!")
+        selection = option_menu(
+            menu_title="Navigation",
+            options=["Accueil", "Photos"],
+            icons=["house", "camera"],
+            menu_icon="cast",
+            default_index=0
+        )
+        if st.button("Déconnexion"):
+            st.session_state["logged_in"] = False
+            st.experimental_rerun()
+
+    if selection == "Accueil":
+        st.title("Page d'accueil")
+        st.write("Bienvenue sur la page d'accueil !")
+    elif selection == "Photos":
+        st.title("Photos de Gizmo")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write("Gizmo qui regarde un film d'horreur")
+            st.image("https://miro.medium.com/v2/resize:fit:720/format:webp/0*yxzaBxdjxYrc0-Ie")
+        with col2:
+            st.write("Gizmo qui pilote une voiture")
+            st.image("https://64.media.tumblr.com/9553ccc7ae6519a2223fcf2a811fc6e2/tumblr_n9k65qnmdG1rvpu8lo2_r1_500.png")
+        with col3:
+            st.write("GIZMO RAMBO !!")
+            st.image("https://pbs.twimg.com/media/F_PXn_VXEAALx_u?format=jpg&name=small")
+
+# Logique principale de l'application
+if st.session_state["logged_in"]:
+    main_menu()
+else:
+    login_page()
